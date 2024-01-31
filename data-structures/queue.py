40a399aee82eeeff679a55c0c404ca721e48d9cc
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-01-30 21:17:33 -0500

    Add size and front methods to queue.py

--- data-structures/queue.py
+++ data-structures/queue.py
@@ -1,6 +1,7 @@
 # queue.py
 
 from collections import deque
+from typing import Any
 
 class EmptyQueueError(Exception):
     """Exception raised when attempting an operation on an empty queue"""
@@ -10,13 +11,26 @@ class Queue():
     def __init__(self) -> None:
         self.queue = deque([])
 
-    def enqueue(self, value: any) -> None:
+    def enqueue(self, value: Any) -> None:
         self.queue.append(value)
 
-    def dequeue(self) -> any:
+    def dequeue(self) -> Any:
         if self.is_empty():
             raise EmptyQueueError("Queue is empty")
         return self.queue.popleft()
     
     def is_empty(self) -> bool:
         return len(self.queue) == 0
+        
+    def size(self) -> int:
+        return len(self.queue)
+        
+    def front(self) -> Any:
+        if self.is_empty():
+            raise EmptyQueueError("Queue is empty")
+        return self.queue[0]
+        
+
+s
+
+
