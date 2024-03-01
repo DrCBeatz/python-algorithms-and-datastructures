ff9690423b6a64765813437443c510f28d5dc557
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-02-29 22:35:33 -0500

    Add type hints to reverse_string.py

--- algorithms/reverse_string.py
+++ algorithms/reverse_string.py
@@ -1,6 +1,9 @@
 # reverse_string.py
 
-def reverse_string(s):
+def reverse_string(s: str) -> str:
+  """
+  Recursive reverse string function
+  """
   if not s:
     return ''
   return reverse_string(s[1:]) + s[0]
