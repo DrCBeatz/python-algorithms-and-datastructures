918ae92f0179cd17e2ed5b14cda35df1e4f503e0
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-03-08 19:34:54 -0500

    Add type hints to alternating_caps.py

--- algorithms/alternating_caps.py
+++ algorithms/alternating_caps.py
@@ -1,6 +1,6 @@
 # alternating_caps.py
 
-def alternating_caps(s, upper=True):
+def alternating_caps(s: str, upper: bool = True) -> str:
     if not s:
         return ''
     return  (s[0].upper() if upper else s[0].lower()) + alternating_caps(s[1:], (upper if s[0] == ' ' else not upper))
